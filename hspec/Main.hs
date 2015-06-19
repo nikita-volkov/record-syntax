083f2591e78cb53ca140df09192acc3bd69d75fb
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-06-19 04:21:37 +0300

    Init tests

--- hspec/Main.hs
+++ hspec/Main.hs
@@ -3,6 +3,15 @@ import Test.Hspec
 
 main = 
   hspec $ do
-    context "" $ do
-      it "" $ do
+    context "Head" $ do
+      it "No module head" $ do
+        pending
+      it "No imports" $ do
+        pending
+    context "Errors" $ do
+      it "Record syntax" $ do
+        pending
+      it "Nested HSE" $ do
+        pending
+      it "Outer HSE" $ do
         pending
